4c23938c0464385056c2194da96cbee469a5254a
atulkumarrai <iamatul1214@gmail.com>
2021-12-16 23:18:44 +0530

    brackets check added

--- Operations.py
+++ Operations.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 class back_Operations():
-    def file_Reader(filename,):
+    def file_Reader(self,filename):
         """ This method just reads the input file from the location"""
         try:
             fname=filename
@@ -11,7 +11,7 @@ class back_Operations():
             print('Exception occured while reading the file \t',str(e))
 
 
-    def create_Review_Column(dataframe):
+    def create_Review_Column(self,dataframe):
         """ This method adds the review column in the existing dataframe"""
         try:
             review_column_name='Auto Review Comments'
@@ -22,7 +22,7 @@ class back_Operations():
             print('Exception occured while creating the review column \t', str(e))
 
 
-    def select_Column_For_Test(dataframe,column_name):
+    def select_Column_For_Test(self,dataframe,column_name):
         try:
             if column_name in dataframe.columns:
                Testable_Column= dataframe[column_name]
@@ -33,3 +33,51 @@ class back_Operations():
             print('Exception occured while selecting column for review test \t', str(e))
 
 
+    # def round_Brackets_Check(self,Testable_Column,iteration_value):
+    def round_Brackets_Check(self, Testable_property):
+        try:
+            count_opening_brackets = 0
+            count_closing_brackets = 0
+            # i=iteration_value
+            # for i in range(len(Testable_Column)):
+            # count_opening_brackets = count_opening_brackets + Testable_Column[i].count('(')
+            # count_closing_brackets = count_closing_brackets + Testable_Column[i].count(')')
+            count_opening_brackets = count_opening_brackets + Testable_property.count('(')
+            count_closing_brackets = count_closing_brackets + Testable_property.count(')')
+
+            if count_opening_brackets == count_closing_brackets:
+                return True
+            else:
+                return False
+
+        except Exception as e:
+            print('Exception occured while checking the round brackets for {0} --- {1}'.format(Testable_property,str(e)))
+
+
+    def square_Brackets_Check(self, Testable_property):
+        try:
+            count_opening_brackets = 0
+            count_closing_brackets = 0
+            # i=iteration_value
+            # for i in range(len(Testable_Column)):
+            # count_opening_brackets = count_opening_brackets + Testable_Column[i].count('(')
+            # count_closing_brackets = count_closing_brackets + Testable_Column[i].count(')')
+            count_opening_brackets = count_opening_brackets + Testable_property.count('[')
+            count_closing_brackets = count_closing_brackets + Testable_property.count(']')
+
+            if count_opening_brackets == count_closing_brackets:
+                return True
+            else:
+                return False
+
+        except Exception as e:
+            print('Exception occured while checking the round brackets for {0} --- {1}'.format(Testable_property,str(e)))
+
+
+    def asterisk_Check(self,Testable_property):
+        try:
+            pass
+
+
+        except Exception as e:
+            print('Exception occured while checking the round brackets for {0} --- {1}'.format(Testable_property, str(e)))
--- Testing_Rough.py
+++ Testing_Rough.py
@@ -1,23 +1,33 @@
 import pandas as pd
-df=pd.read_excel('Resource.xlsm')
+from Operations import back_Operations
+df=pd.read_excel('Testing files/Resource.xlsm')
 print(df.columns)
-properties=df['Properties']
+
+properties=df['Properties'].tolist()
+print(type(properties))
 df['Auto code review']=''
+# for i in range(len(properties)):
+#     count_round_brackets = 0
+#     count_square_brackets = 0
+#     count_round_brackets = count_round_brackets + properties[i].count('(')
+#     count_round_brackets = count_round_brackets+properties[i].count(')')
+#     count_square_brackets = count_square_brackets + properties[i].count('[')
+#     count_square_brackets = count_square_brackets + properties[i].count(']')
+#
+#     if count_round_brackets % 2 == 0 and count_square_brackets % 2 == 0:
+#         df['Auto code review'][i] = "pass"
+#         print(df['Auto code review'])
+#     else:
+#         df['Auto code review'][i] = "xpath broken"
+#         print(df['Auto code review'])
+bo=back_Operations()
 for i in range(len(properties)):
-    count_round_brackets = 0
-    count_square_brackets = 0
-    count_round_brackets = count_round_brackets + properties[i].count('(')
-    count_round_brackets = count_round_brackets+properties[i].count(')')
-    count_square_brackets = count_square_brackets + properties[i].count('[')
-    count_square_brackets = count_square_brackets + properties[i].count(']')
-
-    if count_round_brackets % 2 == 0 and count_square_brackets % 2 == 0:
-        df['Auto code review'][i] = "pass"
-        print(df['Auto code review'])
-    else:
-        df['Auto code review'][i] = "xpath broken"
-        print(df['Auto code review'])
-
+   x= bo.round_Brackets_Check(Testable_property=properties[i])
+   y= bo.square_Brackets_Check(Testable_property=properties[i])
+   if x and y is True:
+       df['Auto code review'][i] = "pass"
+   else:
+       df['Auto code review'][i] = "fail"
 
 print(df['Auto code review'])
 print(df)
